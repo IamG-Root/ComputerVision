5d627ca3452e94d270c0235211550c41a1928851
IamG-Root <vadabenesperiamo@gmail.com>
2025-09-10 16:29:49 +0200

    Updated server/main.py

--- server/main.py
+++ server/main.py
@@ -1,11 +1,18 @@
 import json
 import time
+import signal
 import threading
 import config as cfg
 import paho.mqtt.client as mqtt
 
 modules_data = {}
 lock = threading.Lock()
+
+def signal_handler(sig, frame):
+    print("Interrupt received, stopping MQTT client...")
+    client.loop_stop()
+    client.disconnect()
+    exit(0)
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -23,6 +30,7 @@ def on_message(client, userdata, msg):
 
 
 def init_receiver():
+    global client
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
@@ -32,19 +40,19 @@ def init_receiver():
         print("Connection to broker...")
     except Exception as e:
         print(f"Error occurred during connection: {e}")
-    exit(1)
+        exit(1)
 
-    client.loop_forever()
+    client.loop_start()
 
 
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
     init_receiver()
     while True:
         with lock:
             for module_id, module_data in modules_data.items():
                 print(f"MODULE {module_id}: {json.dumps(module_data)}\n")
         time.sleep(0.1)
-    return
 
 
 if __name__ == "__main__":
